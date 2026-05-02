18d5cff4d7fd2e7b2a9d64215947b8bef0c99fd6
frtook <frtook@hotmail.com>
2026-05-02 07:55:59 +0300

    chore: update api baseURL to point to new environment

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://nooor.tech/api",
+  baseURL: "https://ratil.tryasp.net/api",
 });
 api.interceptors.request.use((config) => {
   return config;
