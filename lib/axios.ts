7b738c8681685179ee93833730894afa7b78e52a
frtook <frtook@hotmail.com>
2026-05-10 13:35:41 +0300

    udpate backend url

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ratil.tryasp.net/api",
+  baseURL: "https://ammarbawazir-002-site1.site4future.com/api",
 });
 api.interceptors.request.use((config) => {
   return config;
